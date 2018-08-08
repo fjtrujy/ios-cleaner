9b8520783087f0a56e1db14d194f3b9c83ade544
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2018-08-08 23:07:01 +0200

    Adding testing methods

--- Cleaner/Cleaner.swift
+++ Cleaner/Cleaner.swift
@@ -10,25 +10,34 @@ import UIKit
 open class Cleaner: NSObject {
     open func cleanEverything() {
         self.cleanDocumentDirectory()
+        self.cleanCacheDirectory()
         self.cleanUserDefault()
     }
     
     open func cleanDocumentDirectory() {
-        let documentsPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        cleanDirectory(.documentDirectory)
+    }
+    
+    open func cleanCacheDirectory() {
+        cleanDirectory(.cachesDirectory)
+    }
+    
+    open func cleanUserDefault() {
+        if let bundleID = Bundle.main.bundleIdentifier {
+            UserDefaults.standard.removePersistentDomain(forName: bundleID)
+        }
+    }
+    
+    func cleanDirectory(_ directory: FileManager.SearchPathDirectory) {
+        let paths = NSSearchPathForDirectoriesInDomains(directory, .userDomainMask, true)
         
-        for folderPath in documentsPaths {
+        for folderPath in paths {
             let folderPathURL = URL.init(string: folderPath)
             let filesInDir = try? FileManager.default.contentsOfDirectory(at: folderPathURL!, includingPropertiesForKeys: nil, options: [])
             
             for case let file as URL in filesInDir!  {
                 try? FileManager.default.removeItem(at: file)
             }
-        }
-    }
-    
-    open func cleanUserDefault() {
-        if let bundleID = Bundle.main.bundleIdentifier {
-            UserDefaults.standard.removePersistentDomain(forName: bundleID)
         }
     }
 }
--- Example/Tests/CleanerTests.swift
+++ Example/Tests/CleanerTests.swift
@@ -2,8 +2,8 @@ import XCTest
 import Cleaner
 
 private struct CleanerTestsConstants{
-    static let FistrumKey : String = "Fistrum"
-    static let SeparatorHeight : CGFloat = 1
+    static let fistrumKey : String = "Fistrum"
+    static let separatorHeight : CGFloat = 1
 }
 
 class CleanerTests: XCTestCase {
@@ -23,20 +23,90 @@ class CleanerTests: XCTestCase {
         // This is an example of a functional test case.
         //Set a value on the user default
         fillUserDefault()
-        cleaner?.cleanDocumentDirectory()
-        let value = UserDefaults.standard.value(forKey: FistrumKey)
-        XCTAssert(value != nil, "The value for the key " + FistrumKey + " should be removed" )
+        cleaner?.cleanUserDefault()
+        let value = UserDefaults.standard.value(forKey: CleanerTestsConstants.fistrumKey)
+        XCTAssert(value == nil, "The value for the key " + CleanerTestsConstants.fistrumKey + " should be removed" )
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testCleanDocumentDirectory() {
+        // This is an example of a functional test case.
+        //Set a value on the user default
+        let imageName = "goku"
+        saveImageIntoDocumentDirectory(fileName: imageName)
+        cleaner?.cleanDocumentDirectory()
+        let image = readImageFromDocumentDirectory(fileName: imageName)
+        
+        XCTAssert(image == nil, "The image " + imageName + " should be removed" )
+    }
+    
+    func testCleanCacheDirectory() {
+        // This is an example of a functional test case.
+        //Set a value on the user default
+        let imageName = "goku"
+        saveImageIntoCacheDirectory(fileName: imageName)
+        cleaner?.cleanCacheDirectory()
+        let image = readImageFromCacheDirectory(fileName: imageName)
+        
+        XCTAssert(image == nil, "The image " + imageName + " should be removed" )
+    }
+
+    // MARK: - Helpers
+    func fillUserDefault() {
+        UserDefaults.standard.set("Chiquito Ipsum", forKey: CleanerTestsConstants.fistrumKey)
+        UserDefaults.standard.synchronize()
+    }
+    
+    func saveImageIntoDocumentDirectory(fileName: String) {
+        return saveImageIntoDirectory(directory: .documentDirectory, fileName: fileName)
+    }
+    
+    func readImageFromDocumentDirectory(fileName: String) -> UIImage? {
+        return readImageFromDirectory(directory: .documentDirectory, fileName: fileName)
+    }
+    
+    func saveImageIntoCacheDirectory(fileName: String) {
+        return saveImageIntoDirectory(directory: .cachesDirectory, fileName: fileName)
+    }
+    
+    func readImageFromCacheDirectory(fileName: String) -> UIImage? {
+        return readImageFromDirectory(directory: .cachesDirectory, fileName: fileName)
+    }
+    
+    func saveImageIntoDirectory(directory: FileManager.SearchPathDirectory, fileName: String) {
+        // get the documents directory url
+        let directory = FileManager.default.urls(for: directory, in: .userDomainMask).first!
+        // choose a name for your image
+        // create the destination file url to save your image
+        let fileURL = directory.appendingPathComponent(fileName + ".jpg")
+        // get your UIImage jpeg data representation and check if the destination file url already exists
+        let image = UIImage.init(named: fileName)
+        if let data = image!.jpegData(compressionQuality: 1.0),
+            !FileManager.default.fileExists(atPath: fileURL.path) {
+            do {
+                // writes the image data to disk
+                try data.write(to: fileURL)
+                print("file saved")
+            } catch {
+                print("error saving file:", error)
+            }
         }
     }
     
-    func fillUserDefault() {
-        UserDefaults.standard.set("Chiquito Ipsum", forKey: FistrumKey)
-        UserDefaults.standard.synchronize()
+    func readImageFromDirectory(directory: FileManager.SearchPathDirectory, fileName: String) -> UIImage? {
+        var image: UIImage?
+        let directory = FileManager.default.urls(for: directory, in: .userDomainMask).first!
+        let fileURL = directory.appendingPathComponent(fileName + ".jpg")
+        let exists = FileManager.default.fileExists(atPath: fileURL.path)
+        
+        if exists {
+            let data = FileManager.default.contents(atPath: fileURL.absoluteString)
+            if (data != nil) {
+                image = UIImage.init(data: data!)
+            }
+            
+        }
+        
+        return image
+        
     }
 }
